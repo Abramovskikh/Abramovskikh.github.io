070e4263457feb1a8f71b68974c174a7e64c240b
Abramovskikh Andrey <djabramovskih@gmail.com>
2019-04-20 07:34:04 +0300

    Работа над канвасом

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,16 @@
-// code js
+const CANVAS = {
+    W: null,
+    H: null,
+    canvas: null,
+    setCanvas: (width, height) => {
+        CANVAS.canvas = document.createElement("canvas");
+        CANVAS.canvas.width = CANVAS.W = width;
+        CANVAS.canvas.height = CANVAS.H = height;
+        document.body.appendChild(CANVAS.canvas);
+    }
+}
 
+window.addEventListener("load", () => {
+    const SW = window.innerWidth, SH = window.innerHeight;
+    CANVAS.setCanvas(SW, SH);
+});
