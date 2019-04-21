02b273ee2aa9bc7bb50acde9a3588738773dbf1b
Abramovskikh Andrey <djabramovskih@gmail.com>
2019-04-21 12:03:36 +0300

    Добавлены новые приемы

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,8 @@ const CANVAS = {
 }
 
 const TOOLS = {
-    isTouched: false,
+    isTouch: false,
+    isTouching: false,
     locationTouch: { x: null, y: null },
     setTouchPosition: (x, y) => {
         TOOLS.locationTouch.x = x;
@@ -41,29 +42,34 @@ window.addEventListener("load", () => {
         c.fillStyle = BG_C;
         c.fillRect(0, 0, innerWidth, innerHeight);
 
-        showPointer(TOOLS.isTouched);
+        drawCircle();
+        iteractivBox( TOOLS.isTouching ? .9 : 1 );
+
+        // Always bottom
+        showPointer(TOOLS.isTouching);
     };
 
     // Animation
     const update = function(time = 0) {
         requestAnimationFrame(update);
-
         draw();
     }
 
     // Touch listeners
     function tStart(e) {
         e.preventDefault();
-        TOOLS.isTouched = true;
+        TOOLS.isTouch = true;
+        TOOLS.isTouching = true;
         tMove(e);
     }
     function tMove(e) {
         e.preventDefault();
+        TOOLS.isTouch = false;
         TOOLS.setTouchPosition(e.touches[0].clientX, e.touches[0].clientY);
     }
     function tEnd(e) {
         e.preventDefault();
-        TOOLS.isTouched = false;
+        TOOLS.isTouching = false;
         TOOLS.setTouchPosition(null, null);
     }
 
@@ -87,5 +93,37 @@ window.addEventListener("load", () => {
         }
     }
 
+    function iteractivBox(scale = 1) {
+        const c = CANVAS.context;
+        let w = 100;
+        c.save();
+        c.translate((innerWidth / 2) - w * scale / 2, (innerHeight / 2) - w * scale / 2);
+        c.scale(scale, scale);
+        c.fillStyle = "rgba(30,121,117, 1)";
+        c.fillRect(0,0,w,w);
+        c.restore();
+    }
+
+    function randomCircle() {
+        const c = CANVAS.context;
+        let w = 10;
+        c.save();
+
+        c.beginPath()
+        c.arc(  Math.random() * innerWidth - w,
+                Math.random() * innerHeight - w,
+                Math.random() * 10 + w,
+                0, Math.PI * 2, false );
+        c.lineWidth = 2;
+        c.strokeStyle = "darkblue";
+        c.stroke();
+        c.restore();
+    }
+    
+    function drawCircle() {
+        for(let i = 0; i < 40; i++) {
+            randomCircle();
+        }
+    }
     update();
 });
